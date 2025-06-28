c00eebef4be38dfbdb2501a116af15e0abe3c267
Hajer Zam <hajer.zmazi2004@gmail.com>
2025-06-28 15:02:57 +0200

    added projects display, has yet to debug

--- script.js
+++ script.js
@@ -110,3 +110,26 @@ document.addEventListener("DOMContentLoaded", () => {
 				});
 			});
 		});
+
+        // Filter projects by category
+document.addEventListener("DOMContentLoaded", () => {
+    const filterButtons = document.querySelectorAll(".filter-btn");
+	const projectCards = document.querySelectorAll(".project-card");
+
+	filterButtons.forEach(btn => {
+		btn.addEventListener("click", () => {
+			const filter = btn.getAttribute("data-filter");
+
+			filterButtons.forEach(b => b.classList.remove("active"));
+			btn.classList.add("active");
+
+			projectCards.forEach(card => {
+				if (filter === "all" || card.dataset.category === filter) {
+					card.style.display = "block";
+				} else {
+					card.style.display = "none";
+				}
+			});
+		});
+	});
+});
